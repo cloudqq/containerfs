ab8108fbdfcf26ddf236f605957fd75ea479925b
bjguoweilong <guowL$0917>
2018-09-25 13:06:17 +0800

    gofmt code

--- proto/packet.go
+++ proto/packet.go
@@ -254,9 +254,9 @@ func (p *Packet) GetOpMsg() (m string) {
 	case OpGetDataPartitionMetrics:
 		m = "OpGetDataPartitionMetrics"
 	case OpBlobStoreGetAllWaterMark:
-		m="OpBlobStoreGetAllWaterMark"
+		m = "OpBlobStoreGetAllWaterMark"
 	case OpNotifyBlobRepair:
-		m="OpNotifyBlobRepair"
+		m = "OpNotifyBlobRepair"
 
 	}
 	return
